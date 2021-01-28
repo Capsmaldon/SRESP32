1f1eaa2921839b19c08dda2f7e5d4999d38622ed
Cai Smaldon <capsmaldon@gmail.com>
2021-01-28 12:36:18 +0000

    Realised there is actually a pin headers file for each development board - it will be much easier to use that

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
-#include "pinHeaders.h"
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PIN_ONBOARD_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PIN_ONBOARD_LED, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  digitalWrite(PIN_ONBOARD_LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
