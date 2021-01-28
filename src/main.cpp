7ba6ff6aa082a5a828b074aea4c7606729c4541f
Cai Smaldon <capsmaldon@gmail.com>
2021-01-28 09:24:19 +0000

    Added a pin define header so that separate pins can be specified but source code can be shared

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
+#include "pinHeaders.h"
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
+  pinMode(PIN_ONBOARD_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
+  digitalWrite(PIN_ONBOARD_LED, HIGH);
   delay(1000);
-  digitalWrite(2, LOW);
+  digitalWrite(PIN_ONBOARD_LED, LOW);
   delay(1000);
 }
