a8e00d89ed1e15f0758a8f9756ba4fe5a32a33c9
Cai Smaldon <capsmaldon@gmail.com>
2021-02-11 21:26:25 +0000

    Finally got some sound out of the thing, sounds horrible but it's something

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "sresp32Display.h"
 #include "sresp32Audio.h"
 
-Sresp32Display sresp32Display;
+//Sresp32Display sresp32Display;
+Sresp32Audio sresp32Audio;
 
 void setup() {
   // put your setup code here, to run once:
-  sresp32Display.initialise();
+  //sresp32Display.initialise();
+  sresp32Audio.initialise();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sresp32Display.tick();
+  //sresp32Display.tick();
+  sresp32Audio.tick();
 }
--- src/sresp32Audio.h
+++ src/sresp32Audio.h
@@ -4,43 +4,56 @@
 #include "driver/i2s.h"
 #include "freertos/queue.h"
 
-
-
-
-
 class Sresp32Audio
 {
-
+public:
     void initialise()
     {
         static const i2s_config_t i2s_config = {
             .mode = static_cast<i2s_mode_t>(I2S_MODE_MASTER | I2S_MODE_TX),
-            .sample_rate = 44100,
+            .sample_rate = sampleRate,
             .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
             .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
             .communication_format = static_cast<i2s_comm_format_t>(I2S_COMM_FORMAT_I2S),
             .intr_alloc_flags = 0, // default interrupt priority
             .dma_buf_count = 8,
-            .dma_buf_len = 64,
+            .dma_buf_len = bufferLength,
             .use_apll = false
         };
 
         static const i2s_pin_config_t pin_config = {
-            .bck_io_num = 26,
-            .ws_io_num = 25,
-            .data_out_num = 27,
+            .bck_io_num = 12,
+            .ws_io_num = 14,
+            .data_out_num = 13,
             .data_in_num = I2S_PIN_NO_CHANGE
         };
 
         i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
         i2s_set_pin(I2S_NUM_0, &pin_config);
         i2s_set_sample_rates(I2S_NUM_0, 44100);
+
+        phaseIncrement = (1.0/static_cast<double>(sampleRate)) * 440.0;
     }
 
-    void run()
+    void tick()
     {
+        for(int i = 0; i < bufferLength; i++)
+        {
+            buffer[i] = phase > 0.5 ? -32768 : 32767;
+            phase += phaseIncrement;
+            if(phase > 1.0) phase -= 1.0;
+        }
 
+        size_t bytesWritten = 0;
+        i2s_write(I2S_NUM_0, buffer, bufferLength, &bytesWritten, 1000);
     }
+
+private:
+const int bufferLength = 64;
+const int sampleRate = 44100;
+double phase = 0;
+double phaseIncrement;
+int16_t buffer[64];
 };
 
 #endif
