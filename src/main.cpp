e216bf5e242a68c4c811080988bf7a882af845e1
Cai Smaldon <capsmaldon@gmail.com>
2021-02-14 12:31:01 +0000

    Setup the audio and display tasks on separate cores - now you can see the display update and hear the audio at the same time.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include "sresp32Display.h"
 #include "sresp32Audio.h"
 
-//Sresp32Display sresp32Display;
+Sresp32Display sresp32Display;
 Sresp32Audio sresp32Audio;
 
+void audioLoop(void* parameters)
+{
+  while(true)
+  {
+    sresp32Audio.tick();
+  }
+}
+
+void displayLoop(void* parameters)
+{
+  while(true)
+  {
+    sresp32Display.tick();
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  //sresp32Display.initialise();
   sresp32Audio.initialise();
+  sresp32Display.initialise();
+
+  xTaskCreatePinnedToCore(audioLoop, "Audio", 10000, NULL, 1, NULL,  0); 
+  xTaskCreatePinnedToCore(displayLoop, "Display", 10000, NULL, 1, NULL,  1); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //sresp32Display.tick();
-  sresp32Audio.tick();
 }
