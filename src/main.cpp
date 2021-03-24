f707fefecab04fe1f2e7a3ac679832e3310c9c61
Cai Smaldon <capsmaldon@gmail.com>
2021-03-24 09:34:24 +0000

    Added a rotary encoder attached to GPIO pins 17 and 5. This currently very roughly controls volume, the task has been added to the scheduler. Needs to be moved over to the implementation file and properly interfaced

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include "sresp32Model.h"
 #include "sresp32Core.h"
 #include "sresp32Audio.h"
+#include "sresp32Pins.h"
 
 std::unique_ptr<Sresp32Model> sresp32Model;
+std::unique_ptr<Sresp32Pins> sresp32Pins;
 std::unique_ptr<Sresp32Core> sresp32Core;
 std::unique_ptr<Sresp32Audio> sresp32Audio;
 
@@ -18,14 +20,20 @@ void coreLoop(void* parameters)
   while(true) sresp32Core->tick();
 }
 
-void setup() {
+void pinLoop(void* parameters)
+{
+  while(true) sresp32Pins->tick();
+}
 
+void setup() {
   sresp32Model = std::unique_ptr<Sresp32Model>(new Sresp32Model());
+  sresp32Pins = std::unique_ptr<Sresp32Pins>(new Sresp32Pins(*sresp32Model));
   sresp32Core = std::unique_ptr<Sresp32Core>(new Sresp32Core(*sresp32Model));
   sresp32Audio = std::unique_ptr<Sresp32Audio>(new Sresp32Audio(*sresp32Model));
 
   xTaskCreatePinnedToCore(audioLoop, "Audio", 10000, NULL, 1, NULL,  0); 
   xTaskCreatePinnedToCore(coreLoop, "Core", 10000, NULL, 1, NULL,  1); 
+  xTaskCreatePinnedToCore(pinLoop, "Pins", 10000, NULL, 1, NULL,  1); 
 }
 
 void loop() {
---
+++ src/sresp32Pins.h
@@ -0,0 +1,42 @@
+#ifndef SRESP_32_PINS_H
+#define SRESP_32_PINS_H
+
+#include <Arduino.h>
+#include <Esp32Encoder.h>
+#include "sresp32Model.h"
+
+class Sresp32Pins
+{
+    public:
+    Sresp32Pins(Sresp32Model &model) : model(model)
+    {
+        Serial.begin(115200);
+        encoder.attachHalfQuad(17, 5);
+    }
+
+    void tick()
+    {
+        int count = encoder.getCount() / 2;
+        if(count)
+        {
+            encoder.clearCount();
+            Serial.println(count);
+
+            Sresp32ModelEntry::LockedDataReference data = model.getEntry(0).access();
+            float& volume = *static_cast<float*>(data.data);
+            volume = volume + (count * 0.1f);
+            if(volume < 0.0f) volume = 0.0f;
+            else if(volume > 1.0f) volume = 1.0f;
+        }
+
+        vTaskDelay(40);
+    }
+
+    private:
+    Sresp32Model &model;
+    ESP32Encoder encoder;
+};
+
+
+#endif
+
